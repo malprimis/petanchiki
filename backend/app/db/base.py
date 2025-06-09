564adcb00ea3055a981cce82155b0a4a40b94d69
Seva Genius <geeneelair@gmail.com>
2025-06-09 21:34:00 +0300

    added new additional __init__.py file to organize imports added enum class Role and common imports for models in base.py
    Signed-off-by: Seva Genius <geeneelair@gmail.com>

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,4 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase
+import enum
+import uuid
+import datetime
+from typing import Annotated
+from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+from sqlalchemy.types import UUID
+
+intpk = Annotated[int, mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)]
+intpkdef = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+updated_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"),
+                                                        onupdate=datetime.datetime.now)]
 
 
 class Base(DeclarativeBase):
@@ -12,3 +24,8 @@ class Base(DeclarativeBase):
                 cols.append(f'{col} = {getattr(self, col)}')
 
         return f'<{self.__class__.__name__}: {', '.join(cols)}>'
+
+
+class Role(enum.Enum):
+    admin = 'admin'
+    user = 'user'
---
+++ backend/app/models/__init__.py
@@ -0,0 +1,9 @@
+from ..db.base import Base, intpk, created_at, updated_at, intpkdef, Role
+from sqlalchemy import ForeignKey, String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+__all__ = [
+    'Base', 'intpk', 'created_at', 'updated_at', 'intpkdef',
+    'ForeignKey', 'Mapped', 'mapped_column', 'relationship',
+    'String', 'Integer', 'Role'
+]

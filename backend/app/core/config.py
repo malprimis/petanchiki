837706e139587ec97aa2aa5e3a621fb182c7e6ed
Seva Genius <geeneelair@gmail.com>
2025-06-15 22:22:23 +0300

    created auth router
    Signed-off-by: Seva Genius <geeneelair@gmail.com>

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -18,6 +18,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
+    SQLALCHEMY_ECHO: bool = True
 
     @property
     def database_url_asyncpg(self):
--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,9 +1,21 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from ..core.config import settings
+from app.core.config import settings
 
 async_engine = create_async_engine(
-    url=settings.database_url_asyncpg
+    url=settings.database_url_asyncpg,
+    echo=settings.SQLALCHEMY_ECHO,
+    future=True
 )
 
-async_sesion_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(async_engine)
+async_sesion_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    autoflush=False
+)
+
+
+async def get_db() -> AsyncSession:
+
+    async with async_sesion_factory() as session:
+        yield session

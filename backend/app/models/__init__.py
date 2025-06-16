579daf89dd5d68dec7d35a96c4e3fbd7d38a563f
Злата <zlata.voronowa@gamil.com>
2025-06-16 20:07:08 +0300

    hi

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .category import Category
-from .group import Group
-from .transaction import Transaction
-from .user import User
-from .user_group import UserGroup
-
-__all__ = ["User", "Group", "UserGroup", "Category", "Transaction"]
